92529f431d54948a2f69f61bd84e561a43198338
Paxton Bigler <paxton.bigler@gmail.com>
2016-07-13 21:54:25 -0600

    latest additions

--- js/messageService.js
+++ js/messageService.js
@@ -10,11 +10,27 @@ angular.module('chatroom').service('messageService', function($http){
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'http://brackcarmony.com:8092/api/chats'
   //Because we're making a POST request, we need a way to tell the server the data we want to give it,
-    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
-  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+    //in your $http call (along with url and method) have a data property which has a value that is equal 
+    // to another object which a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
+  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), 
+  // you can then use .then in your controller.
 
   //postMessage method here
-
+this.postData = function(message) {
+  return $http({
+    method: 'POST',
+    url: 'http://brackcarmony.com:8092/api/chats',
+    data: {text: message}
+  });
+}
 
   //getMessages method here
-});
+
+this.getData = function(){
+  return $http({
+    method: 'GET',
+    url: 'http://brackcarmony.com:8092/api/chats'
+  })
+}
+
+}); //end service
